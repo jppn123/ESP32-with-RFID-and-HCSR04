382a902cf005e67527b5713e9605ff236c0f42e2
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2025-06-04 09:44:13 -0300

    Adicionando logica de adicionar/remover cartoes

--- src/bot.cpp
+++ src/bot.cpp
@@ -6,6 +6,12 @@ WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 unsigned long lastTimeBotRan;
 
+String obterUid(String text, String metodo){
+  int index = text.indexOf(metodo) + String(metodo).length() + 1;
+  String uid = text.substring(index, text.length());
+  return uid;
+}
+
 void handleNewMessages(int numNewMessages) {
   for (int i=0; i<numNewMessages; i++) {
     String chat_id = String(bot.messages[i].chat_id);
@@ -35,6 +41,24 @@ void handleNewMessages(int numNewMessages) {
         bot.sendMessage(chat_id, welcome, "");
     }
 
+    if (strstr(text.c_str(), "cadastrar")) {
+      String uid = obterUid(text, "cadastrar");
+      //TODO implementar a logica de inserir o cartao na lista de cartoes permitidos
+
+      Serial.println("text: " + text);
+      Serial.println("uid: " + uid);
+      //bot.sendMessage(chat_id, "Usuário cadastrado com sucesso", "");
+    }
+
+    if (strstr(text.c_str(), "remover")) {
+      String uid = obterUid(text, "remover");
+      //TODO implementar a logica de remover o cartao da lista de cartoes permitidos
+      
+      Serial.println("text: " + text);
+      Serial.println("uid: " + uid);
+      //bot.sendMessage(chat_id, "Usuário removido com sucesso", "");
+    }
+    
     // if (text == "/on") {
     //   bot.sendMessage(chat_id, "Ligando LED", "");
     //   digitalWrite(LED, HIGH);
