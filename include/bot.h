159627b5f6f147aac8e27bfbb50c1b2be878c36e
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2025-06-08 17:38:35 -0300

    Update bot.h

--- include/bot.h
+++ include/bot.h
@@ -5,8 +5,8 @@
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 
-#define BOTtoken "8105368493:AAENsDUCdCiR7kQc3flK6qZG-1OkZ0-ijRg"  
-#define CHAT_ID "1546484802"
+#define BOTtoken "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
+#define CHAT_ID "xxxxxxxxxxxxx"
 #define DELAY 500
 
 void comunicateToBot();
